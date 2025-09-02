e02858bed2833e29eddad3ef2ed5f131609c4574
csand0222 <csandoval1173@gmail.com>
2025-09-02 12:31:58 -0500

    updated cypress config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      return config
-    },
-    baseUrl: "http://localhost:5173"
-  }
+    baseUrl: "http://localhost:5173",
+    supportFile: false,
+  },
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  video: false,
 });
