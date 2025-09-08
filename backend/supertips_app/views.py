c530d1d40e0e8fea57dbfb694cbcfa049e21ac68
csand0222 <csandoval1173@gmail.com>
2025-09-08 13:01:02 -0500

    WIP - supertips views.py

--- backend/supertips_app/views.py
+++ backend/supertips_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from users_app.views import UserPermissions
+
+
 # Create your views here.
+class SuperTips(UserPermissions):
+    def post(self, request, yard_id):
+        pass
+
+    def get(self, request, yard_id):
+        pass
