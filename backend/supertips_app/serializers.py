5aabbed37b6ceca74704fba366ab660c2e1868ce
csand0222 <csandoval1173@gmail.com>
2025-09-08 22:52:22 -0500

    Refactored OpenAI_app into supertips_app

--- backend/supertips_app/serializers.py
+++ backend/supertips_app/serializers.py
@@ -1,8 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Yard
 
 
-class SuperTipsSerializer(ModelSerializer):
+class SuperTipsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Yard
         fields = "__all__"
+
+
+class SuperTipPromptSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    yard_name = serializers.CharField(max_length=100)
+    zip_code = serializers.CharField(max_length=10)
+    yard_size = serializers.IntegerField(min_value=0)
+    soil_type = serializers.CharField(max_length=20)
+    grass_type = serializers.CharField(max_length=20)
+    watering_interval = serializers.FloatField(min_value=0.5, max_value=7)
+    watering_rate = serializers.FloatField(min_value=0.5, max_value=5)
+    mowing_interval = serializers.IntegerField(min_value=0, max_value=30)
+
+    def validate(self, attrs):
+        for key in ("soil_type", "grass_type"):
+            val = attrs.get(key)
+            if isinstance(val, str) and not val.strip():
+                attrs[key] = "Unknown"
+        return attrs
--- backend/supertips_app/views.py
+++ backend/supertips_app/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+import os
+from openai import OpenAI
+
+from .prompt_helpers import build_user_prompt
 
 from users_app.views import UserPermissions
 
+from yard_app.models import Yard
+from yard_app.serializers import YardSerializer
 
 # Create your views here.
+"""
+Call from frontend to POST 
+[] Get yard info
+[] Get yard prefs
+[] Pass both to serializer
+[] serializer returns dictionary
+[] run serialized_data.is_valid()
+[] store serialized_data.validated_data to 'item'
+[] 
+
+"""
+SYSTEM_PROMPT = (
+    # TODO: ADD SYSTEM PROMPT
+)
+
+
+class AISuperTipsView(UserPermissions):
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    def get(self, request, yard_id):
+        data = request.data.copy()
+        yard_info = Yard.objects.get(id=data.get(""))
+        yard_info = YardSerializer(yard_info).data
+
+        user_prompt = build_user_prompt()
+
+
 class SuperTips(UserPermissions):
     def post(self, request, yard_id):
         pass
